188b086664d4f7c8aa6919086378ef7a05d44421
vact19 <chj6703@gmail.com>
2023-04-23 19:54:35 +0900

    init

---
+++ src/main/java/site/biteme/biteme/BitemeApplication.java
@@ -0,0 +1,13 @@
+package site.biteme.biteme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/biteme/biteme/BitemeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.biteme.biteme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
