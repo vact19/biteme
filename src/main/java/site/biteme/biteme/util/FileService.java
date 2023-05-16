049284f9b45b34600feea1124d047a1ce45464cf
vact19 <chj6703@gmail.com>
2023-05-16 22:11:18 +0900

    Refactor: 변수명 변경, 저장되는 이미지 경로 최적화

--- src/main/java/site/biteme/biteme/util/FileService.java
+++ src/main/java/site/biteme/biteme/util/FileService.java
@@ -17,43 +17,55 @@ import java.util.UUID;
 @Service
 public class FileService {
     @Value("${file.upload.path}")// yml 설정파일
-    private String fileUploadPath;
+    private String fileStorePrefix;
 
     // return fullFilePath
     public List<String> saveAll(List<MultipartFile> multipartFiles){
-        List<String> fullFilePaths = new ArrayList<>();
+        List<String> storedFilePaths = new ArrayList<>();
         for (MultipartFile multipartFile : multipartFiles) {
             // empty Check. type=file 이며 name이 일치한다면, 본문이 비어있어도 MultiPartFile 객체가 생성된다.
             if (multipartFile.isEmpty()){
                 continue;
             }
             String originalFileName = multipartFile.getOriginalFilename();
-            String fullFilePath = createFullFilePath(originalFileName);
+            String storedFilePath = createStoredFilePath(originalFileName);
 
             try {
-                multipartFile.transferTo(new File(fullFilePath));
+                multipartFile.transferTo(new File(fileStorePrefix + storedFilePath));
             } catch (IOException e) {
                 // FileStoreException 발생시키기 전에, IOEXCEPTION 에 대한 로그를 남긴다.
                 log.error("IOEXCEPTION: " + originalFileName + " 저장 불가" );
                 e.printStackTrace();
                 throw new FileStoreException(ErrorCode.FILE_CANNOT_BE_STORED);
             }
-            fullFilePaths.add(fullFilePath);
+            storedFilePaths.add(storedFilePath);
         }
         // 저장한 파일의 경로 리스트를 반환한다.
-        return fullFilePaths;
+        return storedFilePaths;
     }
 
-    private String createFullFilePath(String originalFileName) {
+    private String createStoredFilePath(String originalFileName) {
         String uuid = UUID.randomUUID().toString();
         String ext = extractExt(originalFileName);
 
-        String storedFileName = uuid + "." + ext;
-        return fileUploadPath + storedFileName;
+        return uuid + "." + ext;
+    }
+
+    private String createStoredFilePath(String originalFileName, String prefix) {
+        String uuid = UUID.randomUUID().toString();
+        String ext = extractExt(originalFileName);
+
+        return prefix + "/" + uuid + "." + ext;
     }
 
     private String extractExt(String originalFileName) {
         int pos = originalFileName.lastIndexOf(".");
         return originalFileName.substring(pos +1);
     }
+
+//    public byte[] getByteArr(String url) {
+//        InputStream imageStream = new FileInputStream("C:/Users/woo/myimage/" + "6fee38a0-fc8c-4a94-8f2f-d1bb3a99548e.png");
+//        byte[] bytes = imageStream.readAllBytes();
+//        imageStream.close();
+//    }
 }
