8b2c669181f1200075fe6ff2c7d7379ac8a9561a
vact19 <chj6703@gmail.com>
2023-06-06 15:02:44 +0900

    Update: API 응답 형식

--- src/main/java/site/biteme/biteme/api/question/dto/QuestionListDto.java
+++ src/main/java/site/biteme/biteme/api/question/dto/QuestionListDto.java
@@ -1,5 +1,6 @@
 package site.biteme.biteme.api.question.dto;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import site.biteme.biteme.api.student.dto.StudentIdAndNameDto;
@@ -8,31 +9,41 @@ import site.biteme.biteme.domain.question.Question;
 import site.biteme.biteme.domain.question.component.QuestionState;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class QuestionListDto {
     @Getter
     @Builder
     public static class Response {
-        private Long id;
-        private String title;
-        private QuestionState state;
-        private Category category;
+        private QuestionDto question;
         private StudentIdAndNameDto student;
 
         public static List<Response> of(List<Question> questions) {
             return questions.stream()
                     .map(Response::of)
-                    .collect(java.util.stream.Collectors.toList());
+                    .collect(Collectors.toList());
         }
 
         public static Response of(Question question) {
-            return Response.builder()
+            QuestionDto questionDto = QuestionDto.builder()
                     .id(question.getId())
                     .title(question.getTitle())
                     .state(question.getQuestionState())
                     .category(question.getCategory())
+                    .build();
+            return Response.builder()
+                    .question(questionDto)
                     .student(StudentIdAndNameDto.of(question.getOwnerStudent()))
                     .build();
+        }
+
+        @Getter
+        @Builder(access = AccessLevel.PRIVATE)
+        static class QuestionDto{
+            private Long id;
+            private String title;
+            private QuestionState state;
+            private Category category;
         }
     }
 }
