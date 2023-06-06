67db73e5065c6d3553af36e2852b888dab5f0c92
vact19 <chj6703@gmail.com>
2023-06-06 17:08:27 +0900

    Feat: 설정 - Cors

--- src/main/java/site/biteme/biteme/global/config/WebConfig.java
+++ src/main/java/site/biteme/biteme/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package site.biteme.biteme.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.biteme.biteme.global.interceptor.AuthenticationInterceptor;
@@ -30,6 +31,13 @@ public class WebConfig implements WebMvcConfigurer {
                 ;
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해
+                .allowedOriginPatterns("*")    // 모든 오리진 허용
+                .allowedMethods("*")    // 모든 메소드 허용
+                .maxAge(1800L);         // preflight 캐시
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
