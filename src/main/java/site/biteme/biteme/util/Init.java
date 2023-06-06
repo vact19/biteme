3e183802e4d2e52f99f798b62a6ffe719086b358
vact19 <chj6703@gmail.com>
2023-06-06 14:30:13 +0900

    Init 데이터 추가

--- src/main/java/site/biteme/biteme/util/Init.java
+++ src/main/java/site/biteme/biteme/util/Init.java
@@ -30,6 +30,9 @@ public class Init {
     @Transactional
     @PostConstruct
     public void init() {
+        /**
+         *  Question 1은 답변 2개, Question 2는 답변 0개
+         */
         Student student = Student.builder()
                 .email("ex@email.com")
                 .name("name")
@@ -42,10 +45,21 @@ public class Init {
         student.getPointStatus().increasePoint(PointAmount.ANSWER_ACCEPTED);
         studentRepository.save(student);
 
+
+        Student student2 = Student.builder()
+                .email("ex2@email.com")
+                .name("name2")
+                .password(passwordEncoder.encode("pppp"))
+                .major(Major.BUSINESS)
+                .department(Department.MEDIA)
+                .build();
+        studentRepository.save(student2);
+
         List<String> strings = new ArrayList<>();
-        strings.add("fsd");
-        strings.add("fsdfdf");
-        strings.add("fsdfdffsdf");
+        strings.add("6fee38a0-fc8c-4a94-8f2f-d1bb3a99548e.png");
+        strings.add("3b1e58ae-ef9c-4049-a34a-130f428f6877.png");
+        strings.add("d1dd09b0-a61a-4f25-b230-38eac79c4a46.jpg");
+
         Question question = Question.builder()
                 .title("title")
                 .category(Category.AI)
@@ -55,12 +69,29 @@ public class Init {
                 .build();
         questionRepository.save(question);
 
+        Question question2 = Question.builder()
+                .title("title")
+                .category(Category.AI)
+                .content("content")
+                .imageUrls(strings)
+                .ownerStudent(student2)
+                .build();
+
+        questionRepository.save(question2);
+
         Answer answer = Answer.builder()
                 .content("content")
                 .question(question)
                 .ownerStudent(student)
                 .build();
         answerRepository.save(answer);
+
+        Answer answer2 = Answer.builder()
+                .content("content")
+                .question(question)
+                .ownerStudent(student2)
+                .build();
+        answerRepository.save(answer2);
     }
 
 
