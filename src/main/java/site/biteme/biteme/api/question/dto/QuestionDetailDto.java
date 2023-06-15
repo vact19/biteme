2c0fcca5da0f6ac789f169add432c2e415f2f678
vact19 <chj6703@gmail.com>
2023-06-16 00:46:35 +0900

    Feat: DTO 구조 변경(질문상세조회 모든자료 조회)
    질문상세 조회 API에서 댓글목록, 답변의 댓글목록 등 모든 정보를 한 번에 조회함.
    CommentDto 멤버 클래스에서 Comment 인터페이스 사용

--- src/main/java/site/biteme/biteme/api/question/dto/QuestionDetailDto.java
+++ src/main/java/site/biteme/biteme/api/question/dto/QuestionDetailDto.java
@@ -5,14 +5,17 @@ import lombok.Builder;
 import lombok.Getter;
 import site.biteme.biteme.api.student.dto.StudentIdAndNameDto;
 import site.biteme.biteme.domain.answer.Answer;
+import site.biteme.biteme.domain.answer.comment.AnswerComment;
 import site.biteme.biteme.domain.answer.component.AnswerState;
+import site.biteme.biteme.domain.common.Comment;
 import site.biteme.biteme.domain.question.Question;
+import site.biteme.biteme.domain.question.comment.QuestionComment;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * 7. 질문 상세페이지 조회
+ * 질문 상세페이지 조회
  *   1. 질문
  *     0. 질문 식별자
  *     1. 질문작성자
@@ -43,15 +46,16 @@ public class QuestionDetailDto {
          *  파라미터가 적고 내부 구현이 길면 (Entity 받아서 DTO 매핑하는 등)
          *  정적 메서드 of() 안에서 빌더를 부르는 식으로 구현하기로 했다.
          */
+        // MultipleBagFetchException 때문에 질문의 답변리스트는
         @Builder
         public Response(Question question, List<Answer> answerList){
-            this.question = QuestionDto.of(question, answerList.size());
+            this.question = QuestionDto.of(question, answerList.size(), question.getQuestionComments());
             this.answers = AnswerDto.of(answerList);
         }
 
         @Getter
         @Builder(access = AccessLevel.PRIVATE)
-        public static class QuestionDto{
+        static class QuestionDto{
             private Long id;
             private StudentIdAndNameDto student;
             private String title;
@@ -59,9 +63,12 @@ public class QuestionDetailDto {
             private List<String> images;
             private int answerCount;
             private int commentCount;
+            private List<CommentDto> comments;
 
-            public static QuestionDto of(Question question, int answerCount){
+            static QuestionDto of(Question question, int answerCount, List<QuestionComment> questionComments){
                 int commentCount = question.getQuestionComments().size();
+                List<CommentDto> commentDtos = CommentDto.of(questionComments);
+
                 return QuestionDto.builder()
                         .id(question.getId())
                         .student(StudentIdAndNameDto.of(question.getOwnerStudent()))
@@ -70,8 +77,10 @@ public class QuestionDetailDto {
                         .images(question.getImageUrls())
                         .answerCount(answerCount)
                         .commentCount(commentCount)
+                        .comments(commentDtos)
                         .build();
             }
+
         }
 
         @Getter
@@ -82,24 +91,49 @@ public class QuestionDetailDto {
             private String content;
             private AnswerState state;
             private int commentCount;
+            private List<CommentDto> comments;
 
-            public static List<AnswerDto> of(List<Answer> answerList) {
+            static List<AnswerDto> of(List<Answer> answerList) {
                 return answerList.stream()
                         .map(AnswerDto::of)
                         .collect(Collectors.toList());
             }
 
-            public static AnswerDto of(Answer answer) {
-                int commentCount = answer.getAnswerComments().size();
+            static AnswerDto of(Answer answer) {
+                List<AnswerComment> answerComments = answer.getAnswerComments();
+                int commentCount = answerComments.size();
                 return AnswerDto.builder()
                         .id(answer.getId())
                         .student(StudentIdAndNameDto.of(answer.getOwnerStudent()))
                         .content(answer.getContent())
                         .state(answer.getAnswerState())
                         .commentCount(commentCount)
+                        .comments(CommentDto.of(answerComments))
                         .build();
             }
 
+        }
+
+        @Builder(access = AccessLevel.PRIVATE)
+        @Getter
+        static class CommentDto{
+            private Long id;
+            private StudentIdAndNameDto student;
+            private String content;
+
+            static List<CommentDto> of(List<? extends Comment> comments) {
+                return comments.stream()
+                        .map(CommentDto::of)
+                        .collect(Collectors.toList());
+            }
+
+            static CommentDto of(Comment comment) {
+                return CommentDto.builder()
+                        .id(comment.getId())
+                        .student(StudentIdAndNameDto.of(comment.getOwnerStudent()))
+                        .content(comment.getContent())
+                        .build();
+            }
         }
 
 
--- src/main/java/site/biteme/biteme/util/Init.java
+++ src/main/java/site/biteme/biteme/util/Init.java
@@ -6,10 +6,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import site.biteme.biteme.domain.answer.Answer;
 import site.biteme.biteme.domain.answer.AnswerRepository;
+import site.biteme.biteme.domain.answer.comment.AnswerComment;
+import site.biteme.biteme.domain.answer.comment.AnswerCommentRepository;
 import site.biteme.biteme.domain.common.Category;
 import site.biteme.biteme.domain.common.PointAmount;
 import site.biteme.biteme.domain.question.Question;
 import site.biteme.biteme.domain.question.QuestionRepository;
+import site.biteme.biteme.domain.question.comment.QuestionComment;
+import site.biteme.biteme.domain.question.comment.QuestionCommentRepository;
 import site.biteme.biteme.domain.student.Student;
 import site.biteme.biteme.domain.student.StudentRepository;
 import site.biteme.biteme.domain.student.component.Department;
@@ -26,6 +30,8 @@ public class Init {
     private final PasswordEncoder passwordEncoder;
     private final QuestionRepository questionRepository;
     private final AnswerRepository answerRepository;
+    private final AnswerCommentRepository answerCommentRepository;
+    private final QuestionCommentRepository questionCommentRepository;
 
     @Transactional
     @PostConstruct
@@ -33,17 +39,17 @@ public class Init {
         /**
          *  Question 1은 답변 2개, Question 2는 답변 0개
          */
-        Student student = Student.builder()
+        Student student1 = Student.builder()
                 .email("ex@email.com")
                 .name("name")
                 .password(passwordEncoder.encode("pppp"))
                 .major(Major.COMPUTER)
                 .department(Department.IT)
                 .build();
-        student.getPointStatus().increasePoint(PointAmount.ANSWER_ACCEPTED);
-        student.getPointStatus().increasePoint(PointAmount.ANSWER_ACCEPTED);
-        student.getPointStatus().increasePoint(PointAmount.ANSWER_ACCEPTED);
-        studentRepository.save(student);
+        student1.getPointStatus().increasePoint(PointAmount.ANSWER_ACCEPTED);
+        student1.getPointStatus().increasePoint(PointAmount.ANSWER_ACCEPTED);
+        student1.getPointStatus().increasePoint(PointAmount.ANSWER_ACCEPTED);
+        studentRepository.save(student1);
 
 
         Student student2 = Student.builder()
@@ -60,14 +66,14 @@ public class Init {
         strings.add("3b1e58ae-ef9c-4049-a34a-130f428f6877.png");
         strings.add("d1dd09b0-a61a-4f25-b230-38eac79c4a46.jpg");
 
-        Question question = Question.builder()
+        Question question1 = Question.builder()
                 .title("김효중너무멋있다.")
                 .category(Category.AI)
                 .content("김효중이왜멋있는지에대한설명")
                 .imageUrls(strings)
-                .ownerStudent(student)
+                .ownerStudent(student1)
                 .build();
-        questionRepository.save(question);
+        questionRepository.save(question1);
 
         Question question2 = Question.builder()
                 .title("질문이있어요")
@@ -79,19 +85,45 @@ public class Init {
 
         questionRepository.save(question2);
 
-        Answer answer = Answer.builder()
-                .content("content")
-                .question(question)
-                .ownerStudent(student)
+        Answer answer1 = Answer.builder()
+                .content("질문1-답변1")
+                .question(question1)
+                .ownerStudent(student1)
                 .build();
-        answerRepository.save(answer);
+        answerRepository.save(answer1);
 
         Answer answer2 = Answer.builder()
-                .content("content")
-                .question(question)
+                .content("질문1-답변2")
+                .question(question1)
                 .ownerStudent(student2)
                 .build();
         answerRepository.save(answer2);
+
+        QuestionComment questionComment1 = QuestionComment.builder()
+                .content("질문1-댓글1")
+                .question(question1)
+                .ownerStudent(student2)
+                .build();
+        QuestionComment questionComment2 = QuestionComment.builder()
+                .content("질문1-댓글2")
+                .question(question1)
+                .ownerStudent(student2)
+                .build();
+        questionCommentRepository.save(questionComment1);
+        questionCommentRepository.save(questionComment2);
+
+        AnswerComment answerComment1 = AnswerComment.builder()
+                .content("질문1-답변1-댓글1")
+                .answer(answer1)
+                .ownerStudent(student2)
+                .build();
+        AnswerComment answerComment2 = AnswerComment.builder()
+                .content("질문1-답변1-댓글2")
+                .answer(answer1)
+                .ownerStudent(student2)
+                .build();
+        answerCommentRepository.save(answerComment1);
+        answerCommentRepository.save(answerComment2);
     }
 
 
